37bf23209f7306d0b9b25b72c6f93e966d3cd7a0
root <root@ip-172-31-15-96.ap-south-1.compute.internal>
2022-12-13 08:06:06 +0000

    shdhj

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello, Welcome to Minfy Technology pvt ltd" }');
+    res.send('{ "response": "Hello, Welcome to Minfy Technology pvt ltd..." }');
 });
 
 app.get('/will', function (req, res) {
