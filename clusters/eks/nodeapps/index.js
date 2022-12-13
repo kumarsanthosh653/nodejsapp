9402df80d146f6fb3ae3e35846e97f78950c793c
root <root@ip-172-31-15-96.ap-south-1.compute.internal>
2022-12-13 11:13:56 +0000

    dhdh

---
+++ clusters/eks/nodeapps/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('{ "response": "Hello, Welcome to Minfy Technology pvt ltd1" }');
+});
+
+app.get('/will', function (req, res) {
+    res.send('{ "response": "Hello Welcome to the world and minfy" }');
+});
+app.get('/ready', function (req, res) {
+    res.send('{ "response": " Great!, It works!" }');
+});
+app.listen(process.env.PORT || 3000);
+module.exports = app;
