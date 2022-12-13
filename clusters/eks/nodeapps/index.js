98af9828ed2cb71a624c61362393510f126a1edd
root <root@ip-172-31-15-96.ap-south-1.compute.internal>
2022-12-13 11:14:55 +0000

    shdj

--- clusters/eks/nodeapps/index.js
+++ clusters/eks/nodeapps/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello, Welcome to Minfy Technology pvt ltd1" }');
+    res.send('{ "response": "Hello, Welcome to Minfy Technology pvt ltd11" }');
 });
 
 app.get('/will', function (req, res) {
